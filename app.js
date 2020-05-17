27c51c6c54d1bb87ddf4f8394792bbcc163df77d
Nikola M <mihajlovicn44@gmail.com>
2020-05-17 04:47:03 +0200

    Add messages route to app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const cors = require('cors');
 
 const usersRoute = require('./routes/users');
 const roomsRoute = require('./routes/rooms');
+const messagesRoute = require('./routes/messages');
 
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +16,8 @@ app.use(cors());
 
 app.use('/api/users', usersRoute);
 app.use('/api/rooms', roomsRoute);
+app.use('/api/messages', messagesRoute);
+
 
 mongoose.connect(process.env.DB_CONNECT, {
   useNewUrlParser: true,
